6525cbd07ec84142dcd21c5cfc6842b7026866ed
Devjhonael <dev.jhonael@gmail.com>
2024-12-13 05:37:34 -0500

    panel categoria

--- web/admin.py
+++ web/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Categoria,Producto
+
+admin.site.register(Categoria)
+# admin.site.register(Producto)
+
+@admin.register(Producto)
+class ProductoAdmin(admin.ModelAdmin):
+    # que cosas quiero que se muestren en mi listado de admin
+    list_display = ('nombre','precio','categoria','fecha_registro')
+    list_editable = ['precio',]
+    list_filter=['nombre']
+    search_fields = ['nombre' ] 
